f04b3f20206837a833edc5e3218987f1b8e7094b
jeffwji <and3b6s5y>
2019-08-08 14:56:20 -0400

    Init import

---
+++ README.md
@@ -0,0 +1,47 @@
+## 服务器
+
+H2O 以C/S模式运行，首先需要启动一个服务端：
+
+```bash
+cd ~/Downloads
+unzip h2o-3.24.0.3.zip
+cd h2o-3.24.0.3
+java -jar h2o.jar
+```
+缺省将启动一个服务在 http://localhost:54321
+
+### 认证
+
+缺省的 H2O 服务允许匿名访问,参考该地址设置访问认证: http://docs.h2o.ai/h2o/latest-stable/h2o-docs/starting-h2o.html#authentication-options 
+
+### Cluster
+
+多个 H2O 实例可以组成 Cluster。以下在本地启动一个名为 MyH2OCluster 的 Cluster(同一个 cluster 名称必须相同)，每个节点分配 2G 内存:
+```bash
+$ java -Xmx2g -jar h2o.jar -name MyH2OCluster -port 54321
+$ java -Xmx2g -jar h2o.jar -name MyH2OCluster -port 54322
+$ java -Xmx2g -jar h2o.jar -name MyH2OCluster -port 54323
+```
+
+可以不指定端口，h2o会自动从 54322 开始寻找空闲端口。如果各实例启动在不同主机上， H2O Cluster 支持通过广播（multicast based）或 Flatfile based 两种模式找到对方。
+
+* Multicast: 通过参数 `-network <ip_address>/<mask>` 来启动 h2o.jar。例如：`-network 178.0.0.0/8`
+* Flatfile: 将主机IP地址罗列在一个文本文件中，然后通过 `-flatfile <file_name>` 传递个 h2o.jar
+
+Flatfile 格式如下：
+```
+192.168.1.163:54321
+192.168.1.164:54321
+```
+
+参考地址：
+* http://docs.h2o.ai/h2o/latest-stable/h2o-docs/starting-h2o.html#h2o-options
+* http://docs.h2o.ai/h2o/latest-stable/h2o-docs/starting-h2o.html#clouding-up-cluster-creation
+
+### H2O and Spark/HDFS
+
+H2O 通过名为 Sparkling Water 的组件来将计算投送到 Spark 上去进行分布式运算。(http://docs.h2o.ai/sparkling-water/2.3/latest-stable/doc/index.html)
+
+## 客户端
+
+参考 tests 下得测试案例
---
+++ tests/TestH2O.py
@@ -0,0 +1,79 @@
+import unittest
+import h2o
+
+
+class H2OSimpleTest(unittest.TestCase):
+    """
+    参考：http://docs.h2o.ai/h2o/latest-stable/h2o-docs/starting-h2o.html
+    """
+    def test_h2o_init(self):
+        """
+        init方法首先尝试连接服务端，如果没有可连接的服务器就会在本地启动一个。
+
+        返回信息如下：
+
+        Checking whether there is an H2O instance running at http://localhost:54323 . connected.
+        --------------------------  ------------------------------------------
+        。。。
+        H2O cluster name:           MyH2OCluster
+        H2O cluster total nodes:    3
+        H2O cluster free memory:    6 Gb
+        ...
+        H2O connection url:         http://localhost:54323
+        ...
+        --------------------------  ------------------------------------------
+        """
+        h2o.init(ip='localhost', port='54321',
+                 name='MyH2OCluster'  # Cluster name, 可选. 如果指定了该名称客户端会检查是否与登录的 Cluster 匹配。
+        )
+
+        """
+        航班信息分析案例：
+        
+        allyears2k_headers.zip 是一个测试数据集，包含各年份航班信息，内容如下：
+        
+        Year,Month,DayofMonth,DayOfWeek,DepTime,CRSDepTime,ArrTime,CRSArrTime,UniqueCarrier,FlightNum,TailNum,ActualElapsedTime,CRSElapsedTime,AirTime,ArrDelay,DepDelay,Origin,Dest,Distance,TaxiIn,TaxiOut,Cancelled,CancellationCode,Diverted,CarrierDelay,WeatherDelay,NASDelay,SecurityDelay,LateAircraftDelay,IsArrDelayed,IsDepDelayed
+        1987,10,14,3,741,730,912,849,PS,1451,NA,91,79,NA,23,11,SAN,SFO,447,NA,NA,0,NA,0,NA,NA,NA,NA,NA,YES,YES
+        1987,10,15,4,729,730,903,849,PS,1451,NA,94,79,NA,14,-1,SAN,SFO,447,NA,NA,0,NA,0,NA,NA,NA,NA,NA,YES,NO
+        1987,10,17,6,741,730,918,849,PS,1451,NA,97,79,NA,29,11,SAN,SFO,447,NA,NA,0,NA,0,NA,NA,NA,NA,NA,YES,YES
+        1987,10,18,7,729,730,847,849,PS,1451,NA,78,79,NA,-2,-1,SAN,SFO,447,NA,NA,0,NA,0,NA,NA,NA,NA,NA,NO,NO
+        1987,10,19,1,749,730,922,849,PS,1451,NA,93,79,NA,33,19,SAN,SFO,447,NA,NA,0,NA,0,NA,NA,NA,NA,NA,YES,YES
+        ...
+        
+        1.  载入数据
+        """
+        airlines = h2o.import_file("allyears2k_headers.zip")
+
+        """
+        2. 指定＂因子(factor)＂字段．关于 factor 的解释参考：https://www.zhihu.com/question/48472404
+        """
+        airlines['Year'] = airlines['Year'].asfactor()
+        airlines['Month'] = airlines['Month'].asfactor()
+        airlines['DayOfWeek'] = airlines['DayOfWeek'].asfactor()
+        airlines['Cancelled'] = airlines['Cancelled'].asfactor()
+        airlines['FlightNum'] = airlines['FlightNum'].asfactor()
+
+        """
+        3. 设置要预测的字段和 response 字段
+        """
+        predictors = ['Origin', 'Dest', 'Year', 'UniqueCarrier', 'DayOfWeek', 'Month', 'Distance', 'FlightNum']
+        response = 'IsDepDelayed'
+
+        """
+        4. 分割数据集
+        """
+        train, valid = airlines.split_frame(ratios=[0.8], seed=1234)
+
+        """
+        5. ...
+        """
+        bin_num = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096]
+        label = ['8', '16', '32', '64', '128', '256', '512', '1024', '2048', '4096']
+
+        from h2o.estimators import H2OGradientBoostingEstimator
+        for key, num in enumerate(bin_num):
+            airlines_gbm = H2OGradientBoostingEstimator(nbins_cats=num, seed=1234)
+            airlines_gbm.train(x=predictors, y=response, training_frame=train, validation_frame=valid)
+
+        print(label[key], 'training score', airlines_gbm.auc(train=True))
+        print(label[key], 'validation score', airlines_gbm.auc(valid=True))
